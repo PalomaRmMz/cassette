5485df121fda7e6ee4fc7b627fa699f9992a297f
Paloma Ramírez Meza <p.ramirez.m.8080@gmail.com>
2021-03-15 16:52:42 -0600

    Add colors bars

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Player from "./components/Player";
 import Footer from "./components/Footer";
+import Bars from "./components/Bars/Bars";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Player></Player>
       <Footer></Footer>
+      <Bars></Bars>
     </div>
   );
 }
